2331fea32e047024d8d814d16eab7921dddb53aa
NANDAKUMAR P <164600447+nandakumar08@users.noreply.github.com>
2024-08-07 18:50:13 +0530

    Create github_to_s3.sh

---
+++ github_to_s3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="https://github.com/nandakumar08/Docker-images.git"
+BRANCH="main"
+BUCKET_NAME="nandaproject"
+TEMP_DIR="repo"
+
+# Clean up any existing directory
+rm -rf $TEMP_DIR
+
+# Download the repository
+git clone --branch $BRANCH $REPO_URL $TEMP_DIR
+
+# Upload files to S3
+aws s3 sync $TEMP_DIR s3://$BUCKET_NAME
+
+# Clean up
+rm -rf $TEMP_DIR
